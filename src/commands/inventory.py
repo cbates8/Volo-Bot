8870bfd9a9d8dd7b7c3071d88ad3e0f1bc71b673
cbates8 <cwbates8@gmail.com>
2024-08-20 19:15:23 -0700

    Separate JSON utils

--- src/commands/inventory.py
+++ src/commands/inventory.py
@@ -4,7 +4,7 @@ from discord.ext.commands import Bot, Cog, Context, command, parameter
 
 from constants.paths import INVENTORY_PATH
 from utils.embed import dict_to_embed
-from utils.utils import load_json, write_json
+from utils.json_utils import load_json_async, write_json_async
 
 
 class Inventory(Cog):
@@ -33,7 +33,7 @@ class Inventory(Cog):
             ctx (`Context`): Message context object from Discord
             item (`str`, optional): The name of the inventory item to list. If ommitted, entire inventory will be listed. Defaults to `None`.
         """
-        inventory = await load_json(INVENTORY_PATH)
+        inventory = await load_json_async(INVENTORY_PATH)
 
         if not item:
             embed = dict_to_embed("Inventory", inventory)
@@ -58,7 +58,7 @@ class Inventory(Cog):
             description (`str`, optional): A description of the stored item. Defaults to `None`.
             quantity (`int`, optional): The quantity of the item to store. Defaults to '1'.
         """
-        inventory = await load_json(INVENTORY_PATH)
+        inventory = await load_json_async(INVENTORY_PATH)
 
         if item in inventory.keys():
             inventory[item]["quantity"] += quantity
@@ -67,7 +67,7 @@ class Inventory(Cog):
         else:
             inventory[item] = {"description": description, "quantity": quantity}
 
-        await write_json(INVENTORY_PATH, inventory)
+        await write_json_async(INVENTORY_PATH, inventory)
 
         response = f"Added {quantity} {item} to your inventory."
 
@@ -87,14 +87,14 @@ class Inventory(Cog):
             item (`str`): Item to remove from the inventory
             quantity (`int`, optional): The quantity of items to remove. Defaults to `None` (Removes all items).
         """
-        inventory = await load_json(INVENTORY_PATH)
+        inventory = await load_json_async(INVENTORY_PATH)
 
         if quantity is None or quantity >= inventory[item]["quantity"]:
             del inventory[item]
         else:
             inventory[item]["quantity"] -= quantity
 
-        await write_json(INVENTORY_PATH, inventory)
+        await write_json_async(INVENTORY_PATH, inventory)
 
         response = f"Removed {'all' if quantity is None else quantity} {item} from your inventory."
 
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -58,32 +58,3 @@ def validate_damage_type(valid_types: list[str], input_type: str) -> Union[str, 
     return False
 
 
-#################################
-### Data transformation utils ###
-#################################
-
-
-async def load_json(file_path: str) -> Union[list, dict]:
-    """Open and deserialize a JSON file to a Python object
-
-    Args:
-        file_path (`str`): Path to JSON file
-
-    Returns:
-        `Union[list, dict]`: Deserialized JSON as a Python object
-    """
-    async with aiofiles.open(file_path, mode="r", encoding="utf8") as jsonfile:
-        raw_json = await jsonfile.read()
-    deserialized_json = json.loads(raw_json)
-    return deserialized_json
-
-
-async def write_json(file_path: str, content: Union[list, dict]) -> None:
-    """Serialize a Python object to a JSON string and write to a file
-
-    Args:
-        file_path (`str`): Path to JSON file
-        content (`Union[list, dict]`): Python object to convert to JSON
-    """
-    async with aiofiles.open(file_path, mode="w", encoding="utf8") as jsonfile:
-        await jsonfile.write(json.dumps(content, indent=4))
