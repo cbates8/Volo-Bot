74d6010c7d50258b4706fd2e1671bfa7bdaac893
cbates8 <cwbates8@gmail.com>
2025-04-25 23:26:15 -0700

    Add combat paths

--- src/constants/paths.py
+++ src/constants/paths.py
@@ -8,6 +8,8 @@ CONSTANTS_DIR = "constants"
 DATA_DIR = "data"
 MEME_DIR = f"{DATA_DIR}/memes"  # Do memes count as 'data'?
 UTILS_DIR = "utils"
+COMBAT_DIR = f"{DATA_DIR}/combat"
+SAVED_COMBAT_DIR = f"{COMBAT_DIR}/saved"
 
 
 #########
@@ -20,3 +22,4 @@ INVENTORY_PATH = f"{DATA_DIR}/inventory.json"
 CRIT_TABLE_PATH = f"{DATA_DIR}/critical_hit_table.csv"
 FUMBLE_TABLE_PATH = f"{DATA_DIR}/fumble_table.csv"
 RULES_PATH = f"{DATA_DIR}/rules.json"
+LIVE_COMBAT_PATH = f"{COMBAT_DIR}/combat.json"
